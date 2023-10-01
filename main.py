5e78d07288095273fa0593eb611eb79cfd45c2f4
Valber <64105356+ValberRodr@users.noreply.github.com>
2023-09-30 21:06:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def handle_whatsapp_message():
-    # Get the message from Twilio
-    message = request.json["message"]
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    incoming_message = request.values.get('Body', '')
 
-    # Respond to the message
-    response = jsonify({
-        "body": "Hello, world!"
-    })
+    # Crie uma resposta TwiML
+    twiml_response = MessagingResponse()
 
-    return response, 200
+    # Processar a mensagem recebida e gerar uma resposta
+    if "Olá" in incoming_message:
+        response_message = "Olá! Obrigado por entrar em contato."
+    elif "Como você está?" in incoming_message:
+        response_message = "Estou bem, obrigado! E você?"
+    else:
+        response_message = "Desculpe, não entendi. Por favor, envie uma mensagem válida."
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    twiml_response.message(response_message)
+
+    return str(twiml_response)
+
+if __name__ == '__main__':
+    app.run()
