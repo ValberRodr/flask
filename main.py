fd9c60ae2a69d8d545c7fa5221c92ca48b04bc80
Valber <64105356+ValberRodr@users.noreply.github.com>
2023-09-30 21:00:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    incoming_message = request.values.get('Body', '')
+@app.route('/inicio')
+def ola():
+    return '<h1>Olá Mundo!</h1>'    
 
-    # Crie uma resposta TwiML
-    twiml_response = MessagingResponse()
-
-    # Processar a mensagem recebida e gerar uma resposta
-    if "Olá" in incoming_message:
-        response_message = "Olá! Obrigado por entrar em contato."
-    elif "Como você está?" in incoming_message:
-        response_message = "Estou bem, obrigado! E você?"
-    else:
-        response_message = "Desculpe, não entendi. Por favor, envie uma mensagem válida."
-
-    twiml_response.message(response_message)
-
-    return str(twiml_response)
-
-if __name__ == '__main__':
-    app.run(port=os.getenv("PORT", default=5000))
+app.run()
